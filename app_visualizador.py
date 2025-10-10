de8855971787102353efe96663a7626d1ff48b2c
AlarmasCiateq <lora.la.paz.sapa@gmail.com>
2025-10-10 08:41:29 -0600

    Update app_visualizador.py

--- app_visualizador.py
+++ app_visualizador.py
@@ -13,8 +13,9 @@ fecha_seleccionada = st.date_input(
     max_value=hoy
 )
 
-GITHUB_USER = "tu-usuario"  # ← ¡CAMBIA ESTO!
+GITHUB_USER = "AlarmasCiateq" 
 video_url = f"https://{GITHUB_USER}.github.io/mi-mapa-sectores/hidro-videos/presion_{fecha_seleccionada}.mp4"
 
 st.video(video_url)
+
 st.caption("Los videos se generan diariamente a partir de capturas cada 5 minutos.")
