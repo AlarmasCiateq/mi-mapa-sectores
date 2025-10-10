bd80d1e7b2e5ad14983eaef05d2905f4950f3fcd
AlarmasCiateq <lora.la.paz.sapa@gmail.com>
2025-10-10 10:58:03 -0600

    Update app_visualizador.py

--- app_visualizador.py
+++ app_visualizador.py
@@ -8,45 +8,45 @@ st.title("💧 Evolución de Sectores Hidráulicos")
 
 GITHUB_USER = "alarmasciateq"  # ← tu usuario real
 
-    st.sidebar.markdown(
-        """
-        <div style="
-            position: fixed;
-            top: 10px;
-            right: 15px;
-            width: 220px;
-            background-color: #111111;
-            padding: 8px 12px;
-            border-radius: 10px;
-            box-shadow: 0 2px 8px rgba(0,0,0,0.1);
-            z-index: 9999;  /* Máximo nivel */
-            text-align: center;
-            color: white;
-            font-family: 'Segoe UI', sans-serif;
-            pointer-events: none; /* Evita interferencia con clics */
-            transition: all 0.2s ease;
-        ">
-            <p style='
-                margin: 0; 
-                color: #782e44; 
-                font-size: 1.1em; 
-                font-weight: 600;
-                letter-spacing: 0.5px;
-            '>
-                CIATEQ 💦
-            </p>
-            <p style='
-                color: #aaa; 
-                font-size: 0.65em; 
-                margin: 0; 
-                line-height: 1.2;
-            '>
-                © 2025 Todos los derechos reservados
-            </p>
-        </div>
-        """,
-        unsafe_allow_html=True
-    )
+st.sidebar.markdown(
+    """
+    <div style="
+        position: fixed;
+        top: 10px;
+        right: 15px;
+        width: 220px;
+        background-color: #111111;
+        padding: 8px 12px;
+        border-radius: 10px;
+        box-shadow: 0 2px 8px rgba(0,0,0,0.1);
+        z-index: 9999;  /* Máximo nivel */
+        text-align: center;
+        color: white;
+        font-family: 'Segoe UI', sans-serif;
+        pointer-events: none; /* Evita interferencia con clics */
+        transition: all 0.2s ease;
+    ">
+        <p style='
+            margin: 0; 
+            color: #782e44; 
+            font-size: 1.1em; 
+            font-weight: 600;
+            letter-spacing: 0.5px;
+        '>
+            CIATEQ 💦
+        </p>
+        <p style='
+            color: #aaa; 
+            font-size: 0.65em; 
+            margin: 0; 
+            line-height: 1.2;
+        '>
+            © 2025 Todos los derechos reservados
+        </p>
+    </div>
+    """,
+    unsafe_allow_html=True
+)
 
 hoy = date.today()
 min_fecha = hoy - timedelta(days=30)
@@ -78,4 +78,5 @@ st.markdown(
 )
 
 st.caption("Los videos se generan diariamente a partir de capturas cada 5 minutos.")
+
 
